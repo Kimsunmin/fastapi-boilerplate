fc0d50264b3e3fafa1fee719828e1858bae95a1d
김선민 <smkim@dweax.com>
2026-03-16 09:12:40 +0900

    feat: CORS 미들웨어 추가

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 
 from app.core.config import get_settings
@@ -30,6 +31,7 @@ app = FastAPI(
 )
 
 # 미들웨어 등록
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])  # CORS 미들웨어 등록
 app.middleware("http")(request_id_middleware)  # 요청 ID를 생성하고 응답 헤더에 추가하는 미들웨어 등록
 app.middleware("http")(log_requests)  # 요청과 응답을 로깅하는 미들웨어 등록
 
